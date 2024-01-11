ea5478ea07235145f3b4c77ddeec447a7cb5a8a3
ameda-kevin <amedakevin@gmail.com>
2024-01-11 14:38:53 +0300

    updated with the global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/styles/Globals.styled";
 const  App = () =>{
+  const theme = {
+    colors: {
+      orange:"rgba(255,151,0,1.000)",
+      white:"rgba(240,247,255,1)",
+      green:"rgba(152,219,5,1.000)",
+      navyBlue:"rgba(2,12,27,1)",
+      lightNavyBlue:"rgba(4,22,48,1)",
+      purple:"rgba(58,17,1,1.000)",
+      black: "rgba(0,0,0,1.000)",
+    },
+    mobile:"768px",
+    transition: "all 650ms ease-in-out",
+  };
   return (
-    <div>
+    <>
+    <ThemeProvider theme={theme}>
+    <GlobalStyles />
       App
-    </div>
+      <hr/>
+    </ThemeProvider>
+    </>
   );
 }
 
